d3ea50c672dd4c8a0c54fb05f834af21a7d1d361
Rui Ying <yingrui205@gmail.com>
2019-09-26 23:59:26 +0800

    Add katex support

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,39 @@
 import DOMPurify from "dompurify";
 import hljs from "highlight.js";
 import Marked from "marked";
+import katex from "katex";
+
+const renderer = new Marked.Renderer();
+renderer.code = (code, lang, isEscaped) => {
+  if (lang === "math") {
+    return katex.renderToString(code, { throwOnError: false });
+  } else {
+    return new Marked.Renderer().code(code, lang, isEscaped);
+  }
+};
+renderer.codespan = code => {
+  if (code.length >= 2 && code.startsWith("$") && code.endsWith("$")) {
+    return katex.renderToString(code.slice(1, -1), { throwOnError: false });
+  } else {
+    return new Marked.Renderer().codespan(code);
+  }
+};
 
 Marked.setOptions({
+  highlight: code => {
+    return hljs.highlightAuto(code).value;
+  },
   sanitize: true,
-  sanitizer: DOMPurify.sanitize
+  sanitizer: DOMPurify.sanitize,
+  renderer
 });
 
 declare const preval: any;
+
+const katexCSS = preval`
+  const fs = require('fs')
+  module.exports = fs.readFileSync(require.resolve('../node_modules/katex/dist/katex.min.css'), 'utf8')
+`;
 
 const hljsCSS = preval`
   const fs = require('fs')
@@ -46,6 +72,7 @@ const render: (md: string, highlight?: boolean) => string = (md, highlight) => {
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=1.0"/>
         <style>${baseCSS}</style>
         <style>${mdCSS}</style>
+        <style>${katexCSS}</style>
         <style>${highlight ? hljsCSS : ""}</style>
         <style>${bodyCSS}</style>
       </head>
